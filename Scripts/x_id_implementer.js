4ac44abf3c8dbf908d63d0764f31285e2939ddc1
tamura0 <155610609+tamura0@users.noreply.github.com>
2024-02-11 01:12:36 +0100

    Added a directory for songs and made a slight fix & some changes.

--- Scripts/x_id_implementer.js
+++ Scripts/x_id_implementer.js
@@ -1,10 +1,10 @@
-const MY_X_ID_TEXT_ELEMS_ARR = Array.from(document.getElementsByClassName("my_x_id"));
-const MY_X_LINK_ELEMS_ARR = Array.from(document.getElementsByClassName("my_x_link"));
+const myXIdTextElemsArr = Array.from(document.getElementsByClassName("my_x_id"));
+const myXLinkElemsArr = Array.from(document.getElementsByClassName("my_x_link"));
 
-MY_X_ID_TEXT_ELEMS_ARR.forEach((e) => {
+myXIdTextElemsArr.forEach((e) => {
     e.innerText = MY_X_ID;
 });
 
-MY_X_LINK_ELEMS_ARR.forEach((e) => {
+myXLinkElemsArr.forEach((e) => {
     e.setAttribute("href", `https://x.com/${MY_X_ID}`);
 });
